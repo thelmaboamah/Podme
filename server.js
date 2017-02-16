b8a7c4315d510af6bd8419e2e779bba45f4aae26
klawton1 <klawton1@gmail.com>
2017-02-16 11:56:07 -0800

    connected to localHost

--- server.js
+++ server.js
@@ -1,2 +1,34 @@
 var express = require("express");
 var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(express.static('public'));
+
+
+
+// app.get('/', function homepage(req, res) {
+//   res.sendFile(__dirname + '/views/index.html');
+// });
+
+app.get('/', function homepage(req, res) {
+  res.send("CONNECTED TO LOCALHOST");
+});
+
+
+
+
+
+
+
+
+
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Express server is up and running on http://localhost:3000/');
+});
