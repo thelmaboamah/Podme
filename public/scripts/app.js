488133ec1c63420209b331e9101a747f97138e00
Thelma Boamah <thelma.a.boamah@gmail.com>
2017-02-16 19:51:57 -0800

    load podcast from itunes api with hard coded data in ajax call

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,64 +1,38 @@
 $(document).ready(function(){
 	console.log("jQuery is working.");
 
-	var result = [
-	{
-	"wrapperType": "track",
-	"kind": "podcast",
-	"artistId": 125443881,
-	"collectionId": 1057255460,
-	"trackId": 1057255460,
-	"artistName": "NPR",
-	"collectionName": "NPR Politics Podcast",
-	"trackName": "NPR Politics Podcast",
-	"collectionCensoredName": "NPR Politics Podcast",
-	"trackCensoredName": "NPR Politics Podcast",
-	"artistViewUrl": "https://itunes.apple.com/us/artist/npr/id125443881?mt=2&uo=4",
-	"collectionViewUrl": "https://itunes.apple.com/us/podcast/npr-politics-podcast/id1057255460?mt=2&uo=4",
-	"feedUrl": "https://www.npr.org/rss/podcast.php?id=510310",
-	"trackViewUrl": "https://itunes.apple.com/us/podcast/npr-politics-podcast/id1057255460?mt=2&uo=4",
-	"artworkUrl30": "http://is2.mzstatic.com/image/thumb/Music111/v4/3e/80/5a/3e805aa4-6b21-6c15-b7c6-fcadfb4d3e82/source/30x30bb.jpg",
-	"artworkUrl60": "http://is2.mzstatic.com/image/thumb/Music111/v4/3e/80/5a/3e805aa4-6b21-6c15-b7c6-fcadfb4d3e82/source/60x60bb.jpg",
-	"artworkUrl100": "http://is2.mzstatic.com/image/thumb/Music111/v4/3e/80/5a/3e805aa4-6b21-6c15-b7c6-fcadfb4d3e82/source/100x100bb.jpg",
-	"collectionPrice": 0,
-	"trackPrice": 0,
-	"trackRentalPrice": 0,
-	"collectionHdPrice": 0,
-	"trackHdPrice": 0,
-	"trackHdRentalPrice": 0,
-	"releaseDate": "2017-02-15T21:18:00Z",
-	"collectionExplicitness": "notExplicit",
-	"trackExplicitness": "notExplicit",
-	"trackCount": 189,
-	"country": "USA",
-	"currency": "USD",
-	"primaryGenreName": "News & Politics",
-	"artworkUrl600": "http://is2.mzstatic.com/image/thumb/Music111/v4/3e/80/5a/3e805aa4-6b21-6c15-b7c6-fcadfb4d3e82/source/600x600bb.jpg",
-	"genreIds": [
-	  "1311",
-	  "26",
-	  "1325"
-	],
-	"genres": [
-	  "News & Politics",
-	  "Podcasts",
-	  "Government & Organizations"
-	]
-	}];
+	$.ajax({
+		method: "GET",
+		url: "https://itunes.apple.com/search",
+		dataType: "jsonp",
+		data: {
+			entity: "podcast",
+			attribute: "titleTerm",
+			term: "politics"
+		},
+		success: itunesReqSuccess
 
-	var podcastHtml = `<div class="podcast col-xs-6 col-sm-4 col-md-3">
-				<img role="button" class="img-responsive" src="${result[0].artworkUrl600}" alt="">
+	})
+
+	function itunesReqSuccess(data){
+		var podcastArr = data.results;
+		podcastArr.forEach(function(podcast){
+			renderPodcast(podcast);
+		})
+	}
+
+	function renderPodcast(podcast){
+			var podcastHtml = `<div class="podcast col-xs-6 col-sm-4 col-md-3">
+				<img role="button" class="img-responsive" src="${podcast.artworkUrl600}" alt="">
 				<div class="sub-heading">
-					<h4>${result[0].collectionName}</h4>
-					<i class="fa fa-plus" role="button" aria-hidden="true"></i>
+					<h4>${podcast.collectionName}</h4>
+					<i class="fa fa-plus" role="button" aria-hidden="true" title="Add to PodList"></i>
 				</div>
 			</div>`;
-
-	for (var i = 1; i < 7; i++ ) {
-		$("#podcast-list").prepend(podcastHtml);
+			$("#podcast-list").append(podcastHtml);
 	}
 	
-	var morePodcastInfoHtml = `<div class="podcastIfo"></div>`
+	// var morePodcastInfoHtml = `<div class="podcastIfo"></div>`
 
 	
 
