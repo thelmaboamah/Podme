e69aec033e8264a8ba2974c9b9374a112113f31d
klawton1 <klawton1@gmail.com>
2017-02-16 18:01:52 -0800

    added find podlist by id

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/api', function(req, res){
   res.json({
     info: "Welcome to PodMe API. Here you can find some information about the different endpoints and methods that are supported.",
     gitHub_url: "https://github.com/klawton1/Podme",
-    homepage: "--- REPLACE WITH URL----",
+    homepage: "https://podme.herokuapp.com/",
     endpoints: [
       {method: "GET", path: "/api", description: "Describes all available endpoints"},
       {method: "GET", path: "/api/podlist", description: "find all podlists"}
@@ -32,17 +32,44 @@ app.get('/api', function(req, res){
 
 app.get('/api/podlist', function(req, res){
   db.Podlist.find({}, function(err, pods){
+    if(err){console.log(err);}
     res.json(pods);
+  })
+})
+
+app.get('/api/podlist/:id', function(req, res){
+  var id = req.params.id
+  db.Podlist.findOne({_id: id}, function(err, pod){
+    if(err){console.log(err);}
+    console.log("FOUND PODLIST")
+    res.json(pod);
   })
 })
 
 app.post('/api/podlist', function(req, res){
   var info = req.body;
   db.Podlist.create(info, function(err, pod){
+    if(err){console.log(err);}
     console.log(pod);
     res.json(pod);
   })
 })
+
+app.put('/api/podlist/:id', function(req, res){
+  var id = req.params.id;
+  var edits = {
+    name: req.name || null,
+    description: req.description || null,
+  };
+  console.log("EDITS",edits);
+  db.Podlist.update({_id: id}, edits, function(err, pod){
+    if(err){console.log(err);}
+    console.log("UPDATED PODLIST", pod)
+
+  })
+})
+
+app.delete('/api/podlist/:id', function(req, res){})
 
 
 
