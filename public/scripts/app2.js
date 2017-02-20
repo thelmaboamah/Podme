7e968a2bd38891fd08de788dc90bcf4f6684bd70
Thelma Boamah <thelma.a.boamah@gmail.com>
2017-02-19 23:16:22 -0800

    json load error fixed

--- public/scripts/app2.js
+++ public/scripts/app2.js
@@ -106,7 +106,7 @@ function loadPods(podlists){
     $.ajax({
       method: "DELETE",
       url: url,
-      success: function(){     
+      success: function(json){     
         //if podcast array is length 0, render the add podcast/remove list options
         if (json.podcasts.length == 0){
           parentdiv.append(`<h3><a href="/">Add Podcasts</a> <span>Remove Podlist</span></h3>`)
